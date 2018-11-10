55d9809c6abdc78c2c52463732b6b2ff868a33a6
Tobias Johansson <TobiasJohansson911@gmail.com>
2018-11-10 19:43:44 +0100

    G uppgifter klara

---
+++ Labb2Csharp/Position.cs
@@ -0,0 +1,103 @@
+﻿using System;
+namespace Labb2Csharp
+{
+    public class Position
+    {
+        private int xPosition;
+        public int XPosition
+        {
+            get
+            {
+                return xPosition;
+            }
+            set
+            {
+                xPosition = value >= 0 ? value : 0;
+            }
+        }
+        private int yPosition;
+        public int YPosition
+        {
+            get
+            {
+                return yPosition;
+            }
+            set
+            {
+                yPosition = value >= 0 ? value : 0;
+            }
+        }
+
+        public Position(int xPosition, int yPosition)
+        {
+            YPosition = yPosition;
+            XPosition = xPosition;
+        }
+
+        public double Length()
+        {
+            double length = Math.Sqrt((Math.Pow(XPosition, 2) + Math.Pow(YPosition, 2)));
+            return length;
+        }
+
+        public bool Equals(Position p)
+        {
+            return (p.XPosition == XPosition && p.YPosition == YPosition);
+        }
+
+        public Position Clone()
+        {
+            return new Position(XPosition, YPosition);
+        }
+
+        public override string ToString()
+        {
+            return "(" + XPosition + "," + YPosition + ")";
+        }
+
+        public static bool operator >(Position p1, Position p2)
+        {
+            if (p1.Length() > p2.Length())
+            {
+                return true;
+            }
+            else if (p1.Length() == p2.Length())
+            {
+                return p1.XPosition > p2.XPosition;
+            }
+            return false;
+        }
+
+        public static bool operator <(Position p1, Position p2)
+        {
+            if (p1.Length() < p2.Length())
+            {
+                return true;
+            }
+            else if (p1.Length() == p2.Length())
+            {
+                return p1.XPosition < p2.XPosition;
+            }
+            return false;
+        }
+
+        public static Position operator +(Position p1, Position p2)
+        {
+            return new Position(p1.XPosition + p2.XPosition, p1.YPosition + p2.YPosition);
+        }
+
+        public static Position operator -(Position p1, Position p2)
+        {
+            if (p1 < p2)
+            {
+                return new Position(p2.XPosition - p1.XPosition, p2.YPosition - p1.YPosition);
+            }
+            return new Position(p1.XPosition - p2.XPosition, p1.YPosition - p2.YPosition);
+        }
+
+        public static double operator %(Position p1, Position p2)
+        {
+            return Math.Sqrt(Math.Pow(p1.XPosition - p2.XPosition, 2) + Math.Pow(p1.YPosition - p2.YPosition, 2));
+        }
+    }
+}
---
+++ Labb2Csharp/SortedPosList.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Labb2Csharp
+{
+    public class SortedPosList
+    {
+
+        private List<Position> PositionList { get; set; }
+        public Position this[int index] 
+        { 
+            get
+            {
+                return PositionList[index];
+            } 
+        }
+
+        public SortedPosList()
+        {
+            PositionList = new List<Position>();
+        }
+
+        public int Count()
+        {
+            return PositionList.Count;
+        }
+
+        public void Add(Position pos)
+        {
+            for (int i = 0; i < Count(); i++)
+            {
+                if (PositionList[i].Length() > pos.Length())
+                {
+                    PositionList.Insert(i, pos);
+                    return;
+                }
+            }
+            PositionList.Insert(Count(), pos);
+        }
+
+        public bool Remove(Position pos)
+        {
+            for (int i = 0; i < Count(); i++)
+            {
+                if (pos.XPosition == PositionList[i].XPosition && pos.YPosition == PositionList[i].YPosition)
+                {
+                    PositionList.RemoveAt(i);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public SortedPosList Clone()
+        {
+            SortedPosList posList = new SortedPosList();
+            foreach (Position position in PositionList)
+            {
+                posList.Add(position.Clone());
+            }
+            return posList;
+        }
+
+        public SortedPosList CircleContent(Position centerPos, double radius)
+        {
+            SortedPosList circleContList = new SortedPosList();
+            foreach (Position position in PositionList)
+            {
+                if (Math.Pow((position.XPosition - centerPos.XPosition), 2) + Math.Pow((position.YPosition - centerPos.YPosition), 2) < Math.Pow(radius, 2))
+                {
+                    circleContList.Add(position.Clone());
+                }
+            }
+            return circleContList;
+        }
+
+        public static SortedPosList operator +(SortedPosList sp1, SortedPosList sp2)
+        {
+            SortedPosList sortedPosList = new SortedPosList();
+            LoopThroughAndAdd(sp1, sortedPosList);
+            LoopThroughAndAdd(sp2, sortedPosList);
+            return sortedPosList;
+        }
+
+        private static void LoopThroughAndAdd(SortedPosList sp, SortedPosList newSortedPosList)
+        {
+            foreach (Position position in sp.PositionList)
+            {
+                newSortedPosList.Add(position.Clone());
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Join(",", PositionList);
+        }
+    }
+}
